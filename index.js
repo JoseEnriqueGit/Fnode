c216ed8f7ecce5820cfc3dde41c5e080c39513fe
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-06 13:01:31 -0400

    ch03/01-meadowlark.js

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
+
+const app = express()
+
 const port = process.env.PORT || 3000
 
-function serveStaticFile(res, path, contentType, responseCode = 200) {
-  fs.readFile(__dirname + path, (err, data) => {
-    if(err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' })
-      return res.end('500 - Internal Error')
-    }
-    res.writeHead(responseCode, { 'Content-Type': contentType })
-    res.end(data)
-  })
-}
-
-const server = http.createServer((req,res) => {
-  // normalize url by removing querystring, optional trailing slash, and
-  // making lowercase
-  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
-  switch(path) {
-    case '':
-      serveStaticFile(res, '/public/home.html', 'text/html')
-      break
-    case '/about':
-      serveStaticFile(res, '/public/about.html', 'text/html')
-      break
-    case '/img/logo.png':
-      serveStaticFile(res, '/public/img/logo.png', 'image/png')
-      break
-    default:
-      serveStaticFile(res, '/public/404.html', 'text/html', 404)
-      break
-  }
+app.get('/', (req, res) => {
+  res.type('text/plain')
+  res.send('Meadowlark Travel');
 })
 
-server.listen(port, () => console.log(`server started on port ${port}; ` +
-  'press Ctrl-C to terminate....'))
+app.get('/about', (req, res) => {
+  res.type('text/plain')
+  res.send('About Meadowlark Travel')
+})
+
+// custom 404 page
+app.use((req, res) => {
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
+})
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port}; ` +
+  `press Ctrl-C to terminate.`))
