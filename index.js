519532d715a4d72298c5092f1c91055c1468fd1c
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-07 12:56:48 -0400

    Export functionality from a module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import getFortune from './lib/fortune.js';
+
 const app = express()
 
 // configure Handlebars view engine
@@ -14,19 +16,10 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'))
 
 // !
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-]
 
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', { fortune: randomFortune })
+  res.render('about', { fortune: getFortune() } )
 })
-
 
 // custom 404 page
 app.use((req, res) => {
