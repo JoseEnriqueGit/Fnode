6a931415c58d622bd67e2d7730b5fb4665a2f10e
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-06 21:58:51 -0400

    archivos estáticos desde el directorio public

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require('express')
-
+import express from 'express';
+import { engine } from 'express-handlebars';
 const app = express()
+
+// configure Handlebars view engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.type('text/plain')
-  res.send('Meadowlark Travel');
-})
+app.use(express.static('public'));
 
-app.get('/about', (req, res) => {
-  res.type('text/plain')
-  res.send('About Meadowlark Travel')
-})
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => res.render('about'))
+
 
 // custom 404 page
 app.use((req, res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message)
-  res.type('text/plain')
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(port, () => console.log(
