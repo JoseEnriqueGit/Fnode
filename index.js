5cb49fbab7a3d30fd05d19cc16a86c36d68a55c7
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-07 17:49:39 -0400

    Testing and refactoring

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-import getFortune from './lib/fortune.js';
+import express from "express";
+import { engine } from "express-handlebars";
 
-const app = express()
+// eslint-disable-next-line semi
+import { home, about, notFound, serverError } from "./lib/handlers.js";
+
+const app = express();
 
 // configure Handlebars view engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.render('home'))
+// ! routes
+app.get("/", home);
 
-// !
-
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: getFortune() } )
-})
+app.get("/about", about);
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(notFound);
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(serverError);
 
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`))
+app.listen(port, () =>
+	console.log(
+		`Express started on http://localhost:${port}; ` +
+			`press Ctrl-C to terminate.`
+	)
+);
