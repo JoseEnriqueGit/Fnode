ffd497c93e530daf7977587471f4cac467338159
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-10 20:01:58 -0400

    vacation photo contest

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import bodyParser from "body-parser";
+import multiparty from "multiparty";
 
-import { home, about, notFound, serverError, newsletterSignup, newsletterSignupProcess, newsletterSignupThankYou } from "./lib/handlers.js";
+import {
+	home,
+	about,
+	notFound,
+	serverError,
+	newsletter,
+	newsletterSignup,
+	newsletterSignupProcess,
+	newsletterSignupThankYou,
+	vacationPhotoContestProcess
+} from "./lib/handlers.js";
 import weatherMiddlware from "./lib/middleware/weather.js";
 
 const app = express();
 app.disable("x-powered-by");
 app.set("view cache", true);
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // configure Handlebars view engine
 app.engine(
@@ -33,13 +45,50 @@ app.use(express.static("public"));
 app.use(weatherMiddlware);
 
 // ! routes
+// home
 app.get("/", home);
 
+// about
 app.get("/about", about);
 
-app.get('/newsletter-signup', newsletterSignup)
-app.post('/newsletter-signup/process', newsletterSignupProcess)
-app.get('/newsletter-signup/thank-you', newsletterSignupThankYou)
+// newletter
+app.get("/newsletter", newsletter);
+app.get("/newsletter-signup", newsletterSignup);
+app.post("/newsletter-signup/process", newsletterSignupProcess);
+app.get("/newsletter-signup/thank-you", newsletterSignupThankYou);
+
+// vacation photo contest
+
+app.post("/contest/vacation-photo/:year/:month", (req, res) => {
+	const form = new multiparty.Form();
+	form.parse(req, (err, fields, files) => {
+		if (err) return res.status(500).send({ error: err.message });
+		vacationPhotoContestProcess(req, res, fields, files);
+	});
+});
+
+// app.get("/contest/vacation-photo", vacationPhotoContest);
+// app.get("/contest/vacation-photo-ajax", vacationPhotoContestAjax);
+// app.post("/contest/vacation-photo/:year/:month", (req, res) => {
+// 	const form = new multiparty.Form();
+// 	form.parse(req, (err, fields, files) => {
+// 		if (err) return vacationPhotoContestProcessError(req, res, err.message);
+// 		console.log("got fields: ", fields);
+// 		console.log("and files: ", files);
+// 		vacationPhotoContestProcess(req, res, fields, files);
+// 	});
+// });
+// app.get(
+// 	"/contest/vacation-photo-thank-you",
+// 	vacationPhotoContestProcessThankYou
+// );
+// app.post("/api/vacation-photo-contest/:year/:month", (req, res) => {
+// 	const form = new multiparty.Form();
+// 	form.parse(req, (err, fields, files) => {
+// 		if (err) return api.vacationPhotoContestError(req, res, err.message);
+// 		api.vacationPhotoContest(req, res, fields, files);
+// 	});
+// });
 
 // custom 404 page
 app.use(notFound);
